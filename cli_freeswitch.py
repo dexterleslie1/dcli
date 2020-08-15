2e70581b6b9b13d2ac48683495cd8a3d27075991
dexterleslie@gmail.com <dexterleslie@gmail.com>
2020-08-15 17:05:42 +0800

    Freeswitch installation support --socks5 option

--- cli_freeswitch.py
+++ cli_freeswitch.py
@@ -4,12 +4,16 @@ import os
 
 class FreeswitchCli(object):
     """
-    Cli for managing freeswitch
+    Cli for managing freeswitch.
     """
 
-    def setup(self):
+    def setup(self, socks5=None):
         """
-        Call ansible role_freeswitch_install role to install freeswitch
+        Call ansible role_freeswitch_install role to install freeswitch.
+
+        :param socks5:
+            Socks5 proxy server to accelerate yum install freeswitch.
+            Value should be in format 192.168.1.12:10080
         :return:
         """
 
@@ -18,4 +22,8 @@ class FreeswitchCli(object):
 
         # Call ansible-playbook command with role_freeswitch_install.yml file
         var_command = "ansible-playbook " + var_full_path + "/role_freeswitch_install.yml"
+
+        if socks5:
+            var_command = var_command + " -e socks5=socks5h://" + socks5
+
         cli_common.execute_command(var_command)
