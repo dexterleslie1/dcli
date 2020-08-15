93a45dc8b1421692b4c1357a90dc67bb10e5f59e
dexterleslie@gmail.com <dexterleslie@gmail.com>
2020-08-15 17:57:28 +0800

    Must specific full path of role_openresty_install.yml when call ansible-playbook cli

--- cli_openresty.py
+++ cli_openresty.py
@@ -1,4 +1,5 @@
 import cli_common
+import os
 
 
 class OpenrestyCli(object):
@@ -16,8 +17,11 @@ class OpenrestyCli(object):
         :return:
         """
 
+        # Full path of python file locates in
+        var_full_path = os.path.dirname(os.path.realpath(__file__))
+
         if from_source:
-            var_command = "ansible-playbook role_openresty_install.yml"
+            var_command = "ansible-playbook " + var_full_path + "role_openresty_install.yml"
             cli_common.execute_command(var_command)
         else:
             # TODO: Install openresty from yum repository
