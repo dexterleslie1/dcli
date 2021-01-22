86c0fb1c4b8ccbed9c0bbd090ae834940440c891
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-01-23 04:13:05 +0800

    Support deploying openresty+naxsi

--- cli_openresty.py
+++ cli_openresty.py
@@ -1,21 +1,20 @@
 import cli_common
 import os
+import logging
 
 
 class OpenrestyCli(object):
     """
-    Cli for managing openresty.
+    Cli for managing openresty. OS support: centOS8
     """
 
-    def install(self, from_source=True, remote_hosts="127.0.0.1,", remote_user="root"):
+    def install(self, from_source=True):
         """
         Install openresty.
 
         :param from_source:
             When true install openresty from source code.
             When false install openresty from yum repository.
-        :param remote_hosts
-        :param remote_user
         :return:
         """
 
@@ -23,8 +22,18 @@ class OpenrestyCli(object):
         var_full_path = os.path.dirname(os.path.realpath(__file__))
 
         if from_source:
+            # # Compile openresty on compile machine
+            # logging.info("########################### Compile openresty ##############################")
+            # var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + var_full_path + "/role_openresty_compile.yml"
+            # var_command = var_command + " --user root"
+            # var_command = var_command + " --ask-pass"
+            # cli_common.execute_command(var_command)
+
+            # Deploy openresty to target machine
+            logging.info("########################### Deploy openresty ##############################")
             var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + var_full_path + "/role_openresty_install.yml"
-            var_command = cli_common.concat_command(var_command, remote_hosts, remote_user)
+            var_command = var_command + " --user root"
+            var_command = var_command + " --ask-pass"
             cli_common.execute_command(var_command)
         else:
             # TODO: Install openresty from yum repository
