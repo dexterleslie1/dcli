e56147351f6ef5e781bf672497e2e0f51a1b610f
dexterleslie@gmail.com <dexterleslie@gmail.com>
2021-01-31 22:22:00 +0800

    忽略提交

--- cli_grafana.py
+++ cli_grafana.py
@@ -21,9 +21,15 @@ class GrafanaCli(object):
         if var_install.lower() == "y":
             var_host_prometheus = raw_input("Enter target machine of deploying grafana (example: 192.168.1.20:8080): ")
             var_host_prometheus_user = raw_input("Enter target machine user (default root): ") or "root"
+            # var_grafana_admin = raw_input("Enter grafana admin user or left it empty without changing: ")
+            # var_grafana_admin_password = raw_input("Enter grafana admin user password or left it empty without changing: ")
 
         # Install grafana
         if var_install.lower() == "y":
             var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + var_full_path + "/role_grafana_install.yml"
             var_command = cli_common.concat_command(var_command, var_host_prometheus, var_host_prometheus_user)
+            # if var_grafana_admin:
+            #     var_command = var_command + " -e var_grafana_admin=" + var_grafana_admin
+            # if var_grafana_admin_password:
+            #     var_command = var_command + " -e var_grafana_admin_password=" + var_grafana_admin_password
             cli_common.execute_command(var_command)
