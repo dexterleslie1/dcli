b1f16701615c4f7437cbf4df0649d7558bcc89c0
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-03-29 11:05:25 +0800

    部署openresty提前询问用户所有设置再执行部署步骤

--- cli_openresty.py
+++ cli_openresty.py
@@ -21,24 +21,31 @@ class OpenrestyCli(object):
         # Full path of python file locates in
         var_full_path = os.path.dirname(os.path.realpath(__file__))
 
+        var_compile_locally = "n"
+        var_install_locally = "n"
         if from_source:
             var_compile = input("是否编译openresty？ [y/n]： ")
             var_install = input("安装openresty？ [y/n]： ")
             if var_compile.lower() == "y":
-                # 在编译主机中编译openresty
-                logging.info("########################### 编译openresty ##############################")
-
-                var_install_locally = input("是否本地编译openresty？ [y/n]: ") or "n"
-                if not var_install_locally == "y":
+                var_compile_locally = input("是否本地编译openresty？ [y/n]: ") or "n"
+                if not var_compile_locally == "y":
                     var_host_target = input("编译openresty主机（例如： 192.168.1.20:8080）：")
                     var_host_target_user = input("编译openresty主机的SSH用户（默认 root）：") or "root"
 
-                if var_install_locally == "y":
+            if var_install.lower() == "y":
+                var_install_locally = input("是否本地部署openresty？ [y/n]: ") or "n"
+                if not var_install_locally == "y":
+                    var_host_target = input("部署openresty主机（例如： 192.168.1.20:8080）：")
+                    var_host_target_user = input("部署openresty主机的SSH用户（默认 root）：") or "root"
+
+            if var_compile.lower() == "y":
+                # 在编译主机中编译openresty
+                logging.info("########################### 编译openresty ##############################")
+
+                if var_compile_locally == "y":
                     var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + var_full_path + "/role_openresty_compile.yml --ask-become-pass --connection=local -i 127.0.0.1,"
                 else:
                     var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + var_full_path + "/role_openresty_compile.yml"
-                    # var_command = var_command + " --user root"
-                    # var_command = var_command + " --ask-pass"
                     var_command = cli_common.concat_command(var_command, var_host_target, var_host_target_user)
                 cli_common.execute_command(var_command)
 
@@ -46,17 +53,10 @@ class OpenrestyCli(object):
                 # 部署openresty
                 logging.info("########################### 部署openresty ##############################")
 
-                var_install_locally = input("是否本地部署openresty？ [y/n]: ") or "n"
-                if not var_install_locally == "y":
-                    var_host_target = input("部署openresty主机（例如： 192.168.1.20:8080）：")
-                    var_host_target_user = input("部署openresty主机的SSH用户（默认 root）：") or "root"
-
                 if var_install_locally == "y":
                     var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + var_full_path + "/role_openresty_install.yml --ask-become-pass --connection=local -i 127.0.0.1,"
                 else:
                     var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + var_full_path + "/role_openresty_install.yml"
-                    # var_command = var_command + " --user root"
-                    # var_command = var_command + " --ask-pass"
                     var_command = cli_common.concat_command(var_command, var_host_target, var_host_target_user)
                 cli_common.execute_command(var_command)
         else:
