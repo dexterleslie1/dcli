9f58982d95aad27019ad027601bae29cf3fd9b3b
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-04-13 12:31:25 +0800

    xrdp支持本地安装

--- cli_xrdp.py
+++ cli_xrdp.py
@@ -2,29 +2,43 @@
 
 import cli_common
 import os
+import getpass
 
 
 class XrdpCli(object):
     """
-    Cli for managing xrdp. OS support: centOS8、ubuntu
+    xrdp管理工具。 支持操作系统： centOS8、ubuntu
     """
 
     def install(self):
         """
-        Install xrdp.
+        安装xrdp
 
         :return:
         """
 
         # Full path of python file locates in
-        var_full_path = os.path.dirname(os.path.realpath(__file__))
+        varFullPath = os.path.dirname(os.path.realpath(__file__))
 
-        var_install = input("安装xrdp吗? [y/n]: ")
-        if var_install.lower() == "y":
-            var_host_target = input("请输入目标主机ip地址（例如： 192.168.1.20:8080）： ")
-            var_host_target_user = input("请输入目标主机登录用户（默认： root）： ") or "root"
+        varHostSshIp = ""
+        varHostSshUser = "root"
+        varHostSshPassword = ""
+        varInstallLocally = "n"
+        varSudoPassword = ""
 
-        if var_install.lower() == "y":
-            var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + var_full_path + "/role_xrdp_install.yml"
-            var_command = cli_common.concat_command(var_command, var_host_target, var_host_target_user)
+        varInstall = input("安装xrdp？ [y/n]: ")
+        if varInstall.lower() == "y":
+            varInstallLocally = input("是否本地安装xrdp？ [y/n]: ") or "n"
+            if not varInstallLocally == "y":
+                varHostSshIp = input("请输入目标主机ip地址（例如： 192.168.1.20:8080）： ")
+                varHostSshUser = input("请输入目标主机登录用户（默认： root）： ") or "root"
+                varHostSshPassword = getpass.getpass("输入SSH密码：")
+
+            varSudoPassword = getpass.getpass("输入sudo密码，如果当前为root用户不需要输入：")
+
+        if varInstall.lower() == "y":
+            var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + varFullPath + "/role_xrdp_install.yml"
+            var_command = cli_common.concat_command(var_command, varHostSshIp, varHostSshUser,
+                                                    varHostSshPassword, varSudoPassword,
+                                                    varInstallLocally.lower() == "y")
             cli_common.execute_command(var_command)
