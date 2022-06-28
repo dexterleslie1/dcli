e8fa872583e1a6b7b1c143a423ee9ac592efaaf1
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-06-28 14:41:28 +0800

    忽略提交

--- cli_maven.py
+++ cli_maven.py
@@ -2,6 +2,7 @@
 
 import cli_common
 import os
+import getpass
 
 
 class MavenCli(object):
@@ -22,10 +23,15 @@ class MavenCli(object):
             # Full path of python file locates in
             var_full_path = os.path.dirname(os.path.realpath(__file__))
 
-            var_host_target = input("Enter deploying target machine (example: 192.168.1.20:8080): ")
-            var_host_target_user = input("Enter target machine user (default root): ") or "root"
+            varInstallLocally = input("是否本地安装？ [y/n]: ") or "n"
+
+            var_host_target = input("输入目标主机IP（例如： 192.168.1.20:8080）：")
+            var_host_target_user = input("E输入目标主机SSH用户（默认 root）：") or "root"
+            varSshPassword = getpass.getpass("输入SSH密码：")
+            varSudoPassword = getpass.getpass("输入主机的sudo密码，如果当前为root用户不需要输入：")
 
             var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + var_full_path + "/role_maven_install.yml"
-            var_command = cli_common.concat_command(var_command, var_host_target, var_host_target_user)
+            var_command = cli_common.concat_command(var_command, var_host_target, var_host_target_user, varSshPassword,
+                                                    varSudoPassword, varInstallLocally.lower() == "y")
 
             cli_common.execute_command(var_command)
