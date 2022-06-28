0756a90a0603d096253344bddd70928752e7e6eb
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-06-28 14:49:31 +0800

    忽略提交

--- cli_maven.py
+++ cli_maven.py
@@ -24,10 +24,15 @@ class MavenCli(object):
             var_full_path = os.path.dirname(os.path.realpath(__file__))
 
             varInstallLocally = input("是否本地安装？ [y/n]: ") or "n"
+            var_host_target = ""
+            var_host_target_user = ""
+            varSshPassword = ""
 
-            var_host_target = input("输入目标主机IP（例如： 192.168.1.20:8080）：")
-            var_host_target_user = input("E输入目标主机SSH用户（默认 root）：") or "root"
-            varSshPassword = getpass.getpass("输入SSH密码：")
+            if not varInstallLocally.lower() == "y":
+                var_host_target = input("输入目标主机IP（例如： 192.168.1.20:8080）：")
+                var_host_target_user = input("E输入目标主机SSH用户（默认 root）：") or "root"
+                varSshPassword = getpass.getpass("输入SSH密码：")
+
             varSudoPassword = getpass.getpass("输入主机的sudo密码，如果当前为root用户不需要输入：")
 
             var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + var_full_path + "/role_maven_install.yml"
