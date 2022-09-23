0e4f3f4eb228eda528ad8dffb5aa24db66732dce
dexterleslie@gmail.com <dexterleslie@gmail.com>
2022-09-23 14:48:45 +0800

    忽略提交

--- cli_docker.py
+++ cli_docker.py
@@ -2,6 +2,7 @@
 
 import cli_common
 import os
+import getpass
 
 
 class DockerCli(object):
@@ -20,17 +21,23 @@ class DockerCli(object):
         # Full path of python file locates in
         var_full_path = os.path.dirname(os.path.realpath(__file__))
 
+        var_host_target = ""
+        var_host_target_user = ""
+        varSshPassword = ""
+        varSudoPassword = ""
+
         var_install = input("Install docker? [y/n]: ")
         if var_install.lower() == "y":
             var_install_locally = input("是否本地安装？ [y/n]: ") or "n"
             if not var_install_locally == "y":
                 var_host_target = input("Enter deploying target machine (example: 192.168.1.20:8080): ")
                 var_host_target_user = input("Enter target machine user (default root): ") or "root"
+                varSshPassword = getpass.getpass("输入SSH密码：")
 
-        if var_install.lower() == "y":
-            if var_install_locally == "y":
-                var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + var_full_path + "/role_docker_install.yml --connection=local -i 127.0.0.1,"
-            else:
-                var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + var_full_path + "/role_docker_install.yml"
-                var_command = cli_common.concat_command(var_command, var_host_target, var_host_target_user)
+            varSudoPassword = getpass.getpass("输入主机的sudo密码，如果当前为root用户不需要输入：")
+
+            var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + var_full_path + "/role_docker_install.yml"
+            var_command = cli_common.concat_command(var_command, var_host_target, var_host_target_user, varSshPassword,
+                                                    varSudoPassword, var_install_locally.lower() == "y")
+
             cli_common.execute_command(var_command)
