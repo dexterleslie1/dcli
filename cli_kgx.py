55d605c6e88842e1e81253df947a39d0ccda83e8
dexterleslie@gmail.com <dexterleslie@gmail.com>
2023-05-17 11:03:16 +0800

    role_kgx_install修改为只配置cron docker-compose 重启

--- cli_kgx.py
+++ cli_kgx.py
@@ -6,7 +6,7 @@ import logging
 
 class KgxCli(object):
     """
-    kgx管理工具。支持操作系统： centOS8
+    kgx管理工具。配置kgx crond重启任务。支持操作系统： centOS8
     """
 
     def install(self):
@@ -19,8 +19,6 @@ class KgxCli(object):
         # Full path of python file locates in
         varFullPath = os.path.dirname(os.path.realpath(__file__))
 
-        varCurrentWorkingDirectory = os.getcwd()
-
         varInstall = input("是否安装kgx？ [y/n]： ")
         varInstallLocally = "n"
 
@@ -28,16 +26,6 @@ class KgxCli(object):
         varHostSshUser = None
         varHostSshPassword = ""
         varSudoPassword = ""
-        # 是否安装zookeeper
-        varInstallZookeeper = "n"
-        # 是否安装管理节点单机版redis和数据节点集群版redis
-        varInstallRedis = "n"
-        # 集群主机ip，创建集群命令redis-cli --cluster create使用
-        varRedisClusterBindIp = "127.0.0.1"
-        # 是否安装MariaDB
-        varInstallMariadb = "n"
-        # 是否安装tomcat
-        varInstallTomcat = "n"
 
         if varInstall.lower() == "y":
             varInstallLocally = input("是否本地安装kgx？ [y/n]: ") or "n"
@@ -48,244 +36,9 @@ class KgxCli(object):
 
             varSudoPassword = getpass.getpass("输入sudo密码，如果当前为root用户不需要输入：")
 
-            varInstallMariadb = input("是否安装数据库MariaDB？ [y/n]：") or "n"
-            varInstallZookeeper = input("是否安装Zookeeper？ [y/n]：") or "n"
-            varInstallRedis = input("是否安装管理节点单机版redis和数据节点集群版redis？ [y/n]：") or "n"
-            if varInstallRedis.lower() == "y":
-                varRedisClusterBindIp = input("redis集群对外提供服务ip（默认：127.0.0.1）：") or "127.0.0.1"
-            varInstallTomcat = input("是否安装tomcat？ [y/n]：") or "n"
-            if varInstallTomcat.lower() == "y":
-                # 判断本地是否有lhcclientmanagement.war、lhcclient.war、lhccronb.war、lhcfacadea.war、lhcfacadez.war、lhcfacaded.war
-                if not os.path.exists(varCurrentWorkingDirectory + "/lhcclientmanagement.war"):
-                    raise Exception("当前工作目录不存在lhcclientmanagement.war文件，不能安装tomcat")
-                if not os.path.exists(varCurrentWorkingDirectory + "/lhcclient.war"):
-                    raise Exception("当前工作目录不存在lhcclient.war文件，不能安装tomcat")
-                if not os.path.exists(varCurrentWorkingDirectory + "/lhccronb.war"):
-                    raise Exception("当前工作目录不存在lhccronb.war文件，不能安装tomcat")
-                if not os.path.exists(varCurrentWorkingDirectory + "/lhcfacadea.war"):
-                    raise Exception("当前工作目录不存在lhcfacadea.war文件，不能安装tomcat")
-                if not os.path.exists(varCurrentWorkingDirectory + "/lhcfacadez.war"):
-                    raise Exception("当前工作目录不存在lhcfacadez.war文件，不能安装tomcat")
-                if not os.path.exists(varCurrentWorkingDirectory + "/lhcfacaded.war"):
-                    raise Exception("当前工作目录不存在lhcfacaded.war文件，不能安装tomcat")
-
-        if varInstall.lower() == "y":
-            if varInstallMariadb.lower() == "y":
-                var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + varFullPath + "/role_mariadb_install.yml"
-                var_command = cli_common.concat_command(var_command, varHostSshIp, varHostSshUser,
-                                                        varHostSshPassword, varSudoPassword, varInstallLocally.lower() == "y")
-                cli_common.execute_command(var_command)
-
-            if varInstallZookeeper.lower() == "y":
-                var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + varFullPath + "/role_zookeeper_install.yml"
-                var_command = cli_common.concat_command(var_command, varHostSshIp, varHostSshUser,
-                                                       varHostSshPassword, varSudoPassword,
-                                                       varInstallLocally.lower() == "y")
-
-                var_command = var_command + " -e varPort=2181"
-
-                cli_common.execute_command(var_command)
-
-            if varInstallRedis.lower() == "y":
-                # 管理节点单机版redis
-                var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + varFullPath + "/role_redis_install.yml"
-                var_command = cli_common.concat_command(var_command, varHostSshIp, varHostSshUser,
-                                                       varHostSshPassword, varSudoPassword,
-                                                       varInstallLocally.lower() == "y")
-
-                var_command = var_command + " -e varRedisMode=standalone"
-
-                var_command = var_command + " -e varRedisPort=6379"
-                var_command = var_command + " -e varReboot=false"
-                var_command = var_command + " -e varRedisPassword=\"123456\""
-
-                cli_common.execute_command(var_command)
-
-                # 数据节点集群版redis
-                var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + varFullPath + "/role_redis_install.yml"
-                var_command = cli_common.concat_command(var_command, varHostSshIp, varHostSshUser,
-                                                        varHostSshPassword, varSudoPassword,
-                                                        varInstallLocally.lower() == "y")
-
-                var_command = var_command + " -e varRedisMode=cluster -e varAction=clusterCheckRedisClusterFolderExists"
-                cli_common.execute_command(var_command)
-
-                varClusterCreateStr = ""
-                varClusterNodeCount = 3
-                varClusterFirstNodePort = 6380
-                for varIndex in range(varClusterNodeCount):
-                    logging.info("########################### 部署redis集群第" + str(varIndex+1) + "个节点 ##############################")
-
-                    var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + varFullPath + "/role_redis_install.yml"
-                    var_command = cli_common.concat_command(var_command, varHostSshIp, varHostSshUser,
-                                                            varHostSshPassword, varSudoPassword,
-                                                            varInstallLocally.lower() == "y")
-
-                    varClusterNode = "node" + str(varIndex+1)
-
-                    var_command = var_command + " -e varClusterNode=" + varClusterNode
-                    var_command = var_command + " -e varRedisMode=cluster"
-                    var_command = var_command + " -e varAction=clusterNodeConfig"
-
-                    varClusterNodePort = varClusterFirstNodePort + varIndex
-                    var_command = var_command + " -e varClusterNodePort=" + str(varClusterNodePort)
-
-                    cli_common.execute_command(var_command)
-
-                    varClusterCreateStr = varClusterCreateStr + varRedisClusterBindIp + ":" + str(varClusterNodePort)
-                    if varIndex < varClusterNodeCount-1:
-                        varClusterCreateStr = varClusterCreateStr + " "
-
-                # 调用redis-cli --cluster create创建redis集群
-                var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + varFullPath + "/role_redis_install.yml"
-                var_command = cli_common.concat_command(var_command, varHostSshIp, varHostSshUser,
-                                                        varHostSshPassword, varSudoPassword,
-                                                        varInstallLocally.lower() == "y")
-
-                var_command = var_command + " -e varReboot=false"
-
-                # https://stackoverflow.com/questions/32584112/ansible-spaces-in-command-line-variables
-                var_command = var_command + " -e varRedisMode=cluster -e varAction=clusterCreate -e \"varClusterCreateStr=\'" + varClusterCreateStr + "\'\""
-                cli_common.execute_command(var_command)
-
-            if varInstallTomcat.lower() == "y":
-                # 安装tomcat-lhc-management
-                varTomcatTargetDirectory = "tomcat-lhc-management"
-                varTomcatListenPort = 9999
-
-                var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + varFullPath + "/role_tomcat_install.yml"
-                var_command = cli_common.concat_command(var_command, varHostSshIp, varHostSshUser,
-                                                        varHostSshPassword, varSudoPassword,
-                                                        varInstallLocally.lower() == "y")
-
-                var_catalina_opts = "CATALINA_OPTS=\\\"-server"
-                var_catalina_opts = var_catalina_opts + " -Xmx1g"
-
-                var_catalina_opts = var_catalina_opts + "\\\""
-                var_command = var_command + " -e varCatalinaOpts=\"" + var_catalina_opts + "\""
-
-                var_command = var_command + " -e varTomcatTargetDirectory=" + varTomcatTargetDirectory
-                var_command = var_command + " -e varTomcatListenPort=" + str(varTomcatListenPort)
-                varTomcatShutdownPort = 8005 + (varTomcatListenPort - 8080)
-                var_command = var_command + " -e varTomcatShutdownPort=" + str(varTomcatShutdownPort)
-
-                cli_common.execute_command(var_command)
-
-                # 安装tomcat-lhc-cronb
-                varTomcatTargetDirectory = "tomcat-lhc-cronb"
-                varTomcatListenPort = 8086
-
-                var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + varFullPath + "/role_tomcat_install.yml"
-                var_command = cli_common.concat_command(var_command, varHostSshIp, varHostSshUser,
-                                                        varHostSshPassword, varSudoPassword,
-                                                        varInstallLocally.lower() == "y")
-
-                var_catalina_opts = "CATALINA_OPTS=\\\"-server"
-                var_catalina_opts = var_catalina_opts + " -Xmx1g"
-
-                var_catalina_opts = var_catalina_opts + "\\\""
-                var_command = var_command + " -e varCatalinaOpts=\"" + var_catalina_opts + "\""
-
-                var_command = var_command + " -e varTomcatTargetDirectory=" + varTomcatTargetDirectory
-                var_command = var_command + " -e varTomcatListenPort=" + str(varTomcatListenPort)
-                varTomcatShutdownPort = 8005 + (varTomcatListenPort - 8080)
-                var_command = var_command + " -e varTomcatShutdownPort=" + str(varTomcatShutdownPort)
-
-                cli_common.execute_command(var_command)
-
-                # 安装tomcat-lhc-datanode
-                varTomcatTargetDirectory = "tomcat-lhc-datanode"
-                varTomcatListenPort = 8085
-
-                var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + varFullPath + "/role_tomcat_install.yml"
-                var_command = cli_common.concat_command(var_command, varHostSshIp, varHostSshUser,
-                                                        varHostSshPassword, varSudoPassword,
-                                                        varInstallLocally.lower() == "y")
-
-                var_catalina_opts = "CATALINA_OPTS=\\\"-server"
-                var_catalina_opts = var_catalina_opts + " -Xmx1g"
-
-                var_catalina_opts = var_catalina_opts + "\\\""
-                var_command = var_command + " -e varCatalinaOpts=\"" + var_catalina_opts + "\""
-
-                var_command = var_command + " -e varTomcatTargetDirectory=" + varTomcatTargetDirectory
-                var_command = var_command + " -e varTomcatListenPort=" + str(varTomcatListenPort)
-                varTomcatShutdownPort = 8005 + (varTomcatListenPort - 8080)
-                var_command = var_command + " -e varTomcatShutdownPort=" + str(varTomcatShutdownPort)
-
-                cli_common.execute_command(var_command)
-
-                # 安装tomcat-lhc-facadea
-                varTomcatTargetDirectory = "tomcat-lhc-facadea"
-                varTomcatListenPort = 8082
-
-                var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + varFullPath + "/role_tomcat_install.yml"
-                var_command = cli_common.concat_command(var_command, varHostSshIp, varHostSshUser,
-                                                        varHostSshPassword, varSudoPassword,
-                                                        varInstallLocally.lower() == "y")
-
-                var_catalina_opts = "CATALINA_OPTS=\\\"-server"
-                var_catalina_opts = var_catalina_opts + " -Xmx1g"
-
-                var_catalina_opts = var_catalina_opts + "\\\""
-                var_command = var_command + " -e varCatalinaOpts=\"" + var_catalina_opts + "\""
-
-                var_command = var_command + " -e varTomcatTargetDirectory=" + varTomcatTargetDirectory
-                var_command = var_command + " -e varTomcatListenPort=" + str(varTomcatListenPort)
-                varTomcatShutdownPort = 8005 + (varTomcatListenPort - 8080)
-                var_command = var_command + " -e varTomcatShutdownPort=" + str(varTomcatShutdownPort)
-
-                cli_common.execute_command(var_command)
-
-                # 安装tomcat-lhc-facadez
-                varTomcatTargetDirectory = "tomcat-lhc-facadez"
-                varTomcatListenPort = 8081
-                var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + varFullPath + "/role_tomcat_install.yml"
-                var_command = cli_common.concat_command(var_command, varHostSshIp, varHostSshUser,
-                                                            varHostSshPassword, varSudoPassword,
-                                                            varInstallLocally.lower() == "y")
-
-                var_catalina_opts = "CATALINA_OPTS=\\\"-server"
-                var_catalina_opts = var_catalina_opts + " -Xmx1g"
-
-                var_catalina_opts = var_catalina_opts + "\\\""
-                var_command = var_command + " -e varCatalinaOpts=\"" + var_catalina_opts + "\""
-
-                var_command = var_command + " -e varTomcatTargetDirectory=" + varTomcatTargetDirectory
-                var_command = var_command + " -e varTomcatListenPort=" + str(varTomcatListenPort)
-                varTomcatShutdownPort = 8005 + (varTomcatListenPort - 8080)
-                var_command = var_command + " -e varTomcatShutdownPort=" + str(varTomcatShutdownPort)
-
-                cli_common.execute_command(var_command)
-
-                # 安装tomcat-lhc-facaded
-                varTomcatTargetDirectory = "tomcat-lhc-facaded"
-                varTomcatListenPort = 8080
-                var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + varFullPath + "/role_tomcat_install.yml"
-                var_command = cli_common.concat_command(var_command, varHostSshIp, varHostSshUser,
-                                                        varHostSshPassword, varSudoPassword,
-                                                        varInstallLocally.lower() == "y")
-
-                var_catalina_opts = "CATALINA_OPTS=\\\"-server"
-                var_catalina_opts = var_catalina_opts + " -Xmx1g"
-
-                var_catalina_opts = var_catalina_opts + "\\\""
-                var_command = var_command + " -e varCatalinaOpts=\"" + var_catalina_opts + "\""
-
-                var_command = var_command + " -e varTomcatTargetDirectory=" + varTomcatTargetDirectory
-                var_command = var_command + " -e varTomcatListenPort=" + str(varTomcatListenPort)
-                varTomcatShutdownPort = 8005 + (varTomcatListenPort - 8080)
-                var_command = var_command + " -e varTomcatShutdownPort=" + str(varTomcatShutdownPort)
-
-                cli_common.execute_command(var_command)
-
             var_command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook " + varFullPath + "/role_kgx_install.yml"
             var_command = cli_common.concat_command(var_command, varHostSshIp, varHostSshUser,
                                                     varHostSshPassword, varSudoPassword,
                                                     varInstallLocally.lower() == "y")
 
-            var_command = var_command + " -e \"varCurrentWorkingDirectory=\'" + varCurrentWorkingDirectory + "\'\""
-
             cli_common.execute_command(var_command)
-
-            print("提醒： lhc所有相关组件已经安装完毕，请导入名为lhcmanagement（管理节点数据库）和lhc（数据节点数据库）数据库后重启操作系统既可访问所有服务")
